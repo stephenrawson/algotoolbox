12491f309edae4847cf9c4a683587f501b39a29f
Stephen Rawson <rawson.stephen@gmail.com>
2019-01-08 22:10:47 -0700

    Adding fib python.

--- week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,4 +1,11 @@
 # Uses python3
+def calc_fib_naive(n):
+    if (n <= 1):
+        return n
+
+    return calc_fib(n - 1) + calc_fib(n - 2)
+
+# Uses python3
 def calc_fib(n):
     if n == 0:
         return 0
@@ -12,4 +19,4 @@ n = int(input())
 print(calc_fib(n))
 
 
-# Good job! (Max time used: 0.06/5.00, max memory used: 9617408/536870912.)
+#Good job! (Max time used: 0.06/5.00, max memory used: 9617408/536870912.)

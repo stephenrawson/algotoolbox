52e063c17e98a26c0ac8c436133e16c0e420e1ed
Stephen Rawson <rawson.stephen@gmail.com>
2019-01-08 21:52:38 -0700

    Adding week 2 assignment.

---
+++ week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -0,0 +1,15 @@
+# Uses python3
+def calc_fib(n):
+    if n == 0:
+        return 0
+    arr = [0] * (n+1)
+    arr[1] = 1
+    for i in range(2, n+1):
+        arr[i] = arr[i-1] + arr[i-2]
+    return arr[n]
+
+n = int(input())
+print(calc_fib(n))
+
+
+# Good job! (Max time used: 0.06/5.00, max memory used: 9617408/536870912.)
